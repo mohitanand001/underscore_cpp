3fd9dc5ac2b49db0ac36e33a94a8e29019f43fa4
mohit <anand.mohit001@gmail.com>
2018-08-24 14:31:59 +0530

    Added underscore headers

---
+++ underscore.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+template <typename Iterator, typename Function>
+void each(Iterator begin, Iterator end, Function function);
+
+template <typename Iterator, typename Function>
+void transform(Iterator first_begin, Iterator first_end, Iterator second_begin, Function function);
+
+template <typename Collection, typename Function>
+Collection filter_accept(Collection &collection, Function function);
+
+template <typename Collection, typename Function>
+Collection filter_reject(Collection &collection, Function function);
